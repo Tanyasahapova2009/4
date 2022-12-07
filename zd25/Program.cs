4395d94f1613ff6638fd5057a3bccfcae343d268
Tanyasahapova2009 <tanyasahapova@mail.ru>
2022-12-07 12:04:03 +0300

    25

---
+++ zd25/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+
+Console.WriteLine("ВВедите основание степени а=");
+
+ int a = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("введите показатель степени в=");
+ int b = Convert.ToInt32(Console.ReadLine());
+float c = MathF.Pow(a, b);
+
+Console.WriteLine($"число a возведенное в степень в {c}");
